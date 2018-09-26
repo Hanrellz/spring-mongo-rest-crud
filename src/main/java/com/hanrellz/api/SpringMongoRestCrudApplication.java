db9f6a489361f7fcc407155e86c3556b3a9c8dba
Marco Aurelio de Quadros <marco.quadros@amcom.com.br>
2018-09-26 00:15:51 -0300

    initial commit

---
+++ src/main/java/com/hanrellz/api/SpringMongoRestCrudApplication.java
@@ -0,0 +1,12 @@
+package com.hanrellz.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoRestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoRestCrudApplication.class, args);
+	}
+}
